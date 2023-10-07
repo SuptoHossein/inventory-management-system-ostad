3947ca5f3548c6641ca0841944c2be92737a334a
SuptoHossein <supto.hossein@gmail.com>
2023-10-07 21:42:49 +0600

    first commit

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,56 @@
+<script setup>
+import {ref} from 'vue'
+import { VSkeletonLoader } from 'vuetify/labs/VSkeletonLoader'
+import { createVuetify } from 'vuetify/lib/framework.mjs';
+
+createVuetify({
+    components : {
+        createVuetify
+    }
+});
+
+    const loading = ref(true);
+
+    setTimeout(()=>{
+        loading.value = false
+    },1000);
+
+</script>
+
+<template>
+    <v-card class="ma-5">
+        <v-card-item>
+            <v-row>
+                <v-col>
+                    <v-skeleton-loader
+                    class="mx-auto border"
+                    type="image"
+                    v-if="loading"
+                    >
+                    </v-skeleton-loader>
+
+                    <div class="bg-green pa-5" v-else>
+                        One of three columns
+                    </div>
+                </v-col>
+
+                <v-col>
+                    <v-skeleton-loader
+                    class="mx-auto border"
+                    type="image"
+                    v-if="loading"
+                    >
+                    </v-skeleton-loader>
+
+                    <div class="bg-green pa-5" v-else>
+                        One of three columns
+                    </div>
+                </v-col>
+            </v-row>
+        </v-card-item>
+    </v-card>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Layouts/Header.vue
@@ -0,0 +1,34 @@
+<script setup>
+
+</script>
+
+<template>
+    <v-app-bar title="MY APP" :elevation="1">
+            <template v-slot:append>
+                <v-menu transition="slide-y-reverse-transition" width="180">
+                    <template v-slot:activator="{props}">
+                        <v-avatar v-bind="props" class="mr-3" image="https://randomuser.me/api/portraits/men/85.jpg"></v-avatar>
+                    </template>
+
+                    <v-list>
+                        <v-list-item>
+                        <v-list-item-title>Click Me1</v-list-item-title>
+                        </v-list-item>
+
+                        <v-list-item>
+                        <v-list-item-title>Click Me1</v-list-item-title>
+                        </v-list-item>
+
+                        <v-list-item>
+                        <v-list-item-title>Click Me1</v-list-item-title>
+                        </v-list-item>
+                    </v-list>
+
+                </v-menu>
+            </template>
+        </v-app-bar>
+</template>
+
+<style scoped>
+
+</style>
